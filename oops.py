2c5a9e10d2575c91cafad74fe2d124ddeb3b2f2d
jayakrishnanare <jayakrishananare@gmail.com>
2025-09-04 20:53:45 +0530

    init

---
+++ oops.py
@@ -0,0 +1,10 @@
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    def drive(self):
+        print(f"{self.brand} {self.model} is driving")
+
+c = Car("Tesla", "Model 3")
+c.drive()
